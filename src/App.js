d8a7e3247d5a3b8c6bbd05aa0f7dfb287e4c16af
Andres Joel Carrillo Rodriguez <andresjoe24@gmail.com>
2019-07-08 17:52:32 -0500

    Integracion de redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Provider} from "react-redux"
+import store from "./store"
+import Jugadores from "./components/jugadores"
+import EquipoSeleccionado from './components/EquipoSeleccionado';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+//<EquipoSeleccionado></EquipoSeleccionado>
+
+const App = () => (
+  <Provider store={store}>
+    <main>
+      <h2>Administrador</h2>
+      <Jugadores></Jugadores>
+      <EquipoSeleccionado></EquipoSeleccionado>
+    </main>
+  </Provider>
+  
+) 
 
 export default App;
